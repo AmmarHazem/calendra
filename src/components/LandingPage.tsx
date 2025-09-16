c8a1a278a2e743e3549e030cbc2a0ace81740352
Ammar <ammar@ovasave.com>
2025-09-16 22:54:40 +0400

    adding public layout and navbars

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,7 +7,7 @@ const LandingPage: FC = () => {
   return (
     <div className="flex items-center p-10 gap-24 max-md:flex-col">
       <section className="flex flex-col items-center">
-        <Calendar />
+        <Calendar size={100} className="mb-4" />
         <h1>Your time perfectly planned</h1>
         <div className="flex flex-col gap-4 mt-4">
           <GoogleSignInButton />
---
+++ src/components/PublicNavBar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const PublicNavBar: FC = () => {
+  return (
+    <nav className="flex justify-between items-center fixed z-50 w-full h-28 bg-gray-300 px-10 gap-4 shadow-2xl">
+      PublicNavBar
+    </nav>
+  );
+};
+
+export default PublicNavBar;
